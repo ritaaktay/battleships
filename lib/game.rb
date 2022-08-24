e92080c65a69964d1dd3f6ba805bb8543a1ffc22
ritaaktay <ritaaktay@gmail.com>
2022-08-24 11:16:42 +0100

    added tests for ship placement, when ship out of index and when index already occupied

--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,49 @@
 class Game
   def initialize(ships:, rows:, cols:)
-    @ships = ships
+    @unplaced_ships = ships
     @board = Array.new(rows) {Array.new(cols, ".")}
   end
 
-  attr_reader :ships, :board
+  attr_reader :unplaced_ships, :board
 
-  def place_ship(row:, col:, ship:, dir:)
-    if dir == "v"
-      for i in row-1...row+ship-1 do @board[i][col-1] = "S" end
-    elsif dir == "h" 
-      for i in col-1...col+ship-1 do @board[row-1][i] = "S" end
+  def print
+    for row in @board
+      p row
     end
-    remove_ship(ship)
   end
 
-  def remove_ship(ship)
-    @ships.delete_at(@ships.index(ship))
+  def place_ship(row:, col:, ship:, dir:)
+    case dir
+    when "v"
+      check_index(row: row, col: col, ship: ship, dir: dir)
+      for i in row...row+ship do
+        @board[i][col] = "S" 
+      end
+    when "h"
+      check_index(row: row, col: col, ship: ship, dir: dir)
+      for i in col...col+ship do 
+        @board[row][i] = "S" 
+      end
+    end
+    remove_placed_ship(ship)
+  end
+
+  def check_index(row:, col:, ship:, dir:)
+    case dir
+    when "v"
+      for i in row...row+ship do
+        fail "Ship out of index" if !@board[i] || !@board[i][col]
+        fail "Index occupied" if @board[i][col] == "S" 
+      end
+    when "h"
+      for i in col...col+ship do 
+        fail "Ship out of index" if !@board[row] || !@board[row][i]
+        fail "Index occupied" if @board[row][i] == "S" 
+      end
+    end
+  end
+
+  def remove_placed_ship(ship)
+    @unplaced_ships.delete_at(@unplaced_ships.index(ship))
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,34 +3,54 @@ require 'game'
 RSpec.describe Game do
   it "initializes with an array of unplaced ships" do
     game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
-    expect(game.ships).to eq [5, 4, 3, 3, 2]
+    expect(game.unplaced_ships).to eq [5, 4, 3, 3, 2]
   end
 
   it "places vertical ship on board starting from top left index" do
     game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
-    ships = game.ships.length
+    board, ships = game.board.clone, game.unplaced_ships.length
     game.place_ship(row: 4,col: 4, ship: 2, dir: "v")
-    expect(game.board[3][3]).to eq "S"
-    expect(game.board[4][3]).to eq "S"
-    expect(ships - game.ships.length).to eq 1
-  end
-
-  it "places horizontal ship on board starting from top left index" do
-    game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
-    board, ships = game.board.clone, game.ships.length
-    game.place_ship(row: 4,col: 4, ship: 2, dir: "h")
     for r in 0...10
       for c in 0...10
-        if [3,4].include?(c) && r == 3
-          expect(game.board[3][c]).to eq "S"
+        if [4,5].include?(r) && c == 4
+          expect(game.board[4][c]).to eq "S"
         else
           expect(game.board[r][c]).to eq board[r][c]
         end
       end
     end
-    expect(ships - game.ships.length).to eq 1
+    expect(ships - game.unplaced_ships.length).to eq 1
   end
 
-  it "does not place ship on board if out of index" do
+  it "places horizontal ship on board starting from top left index" do
+    game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
+    board, ships = game.board.clone, game.unplaced_ships.length
+    game.place_ship(row: 4,col: 4, ship: 2, dir: "h")
+    for r in 0...10
+      for c in 0...10
+        if [4,5].include?(c) && r == 4
+          expect(game.board[4][c]).to eq "S"
+        else
+          expect(game.board[r][c]).to eq board[r][c]
+        end
+      end
+    end
+    expect(ships - game.unplaced_ships.length).to eq 1
+  end
+
+  it "does not place ship on board if out of index horizontally" do
+    game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
+    expect{  game.place_ship(row: 8,col: 10, ship: 4, dir: "h")  }.to raise_error "Ship out of index"
+  end
+
+  it "does not place ship on board if out of index vertically" do
+    game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
+    expect{  game.place_ship(row: 7,col: 7, ship: 4, dir: "v")  }.to raise_error "Ship out of index"
+  end
+
+  it "does not place ship on board if index is already occupied" do
+    game = Game.new(ships: [5,4,3,3,2], rows: 10, cols: 10)
+    game.place_ship(row:4, col:4, ship: 5, dir: "h")
+    expect{  game.place_ship(row: 3,col: 6, ship: 4, dir: "v")  }.to raise_error "Index occupied"
   end
 end
